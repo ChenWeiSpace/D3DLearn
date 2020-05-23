0ee9ef5374a9221d1923bd8bef6f7bf7d2e9c288
chenwei <chenweiuse@outlook.com>
2020-05-23 15:57:05 +0800

    添加球体

---
+++ D3DMainWidow/D3DMainWidow/Sphere/SphereObject.h
@@ -0,0 +1,11 @@
+#ifndef SphereObject_h__
+#define SphereObject_h__
+#include "..\AbstractVisualObject.h"
+
+
+class SphereObject : public StaticAbstractVisualObject
+{
+
+};
+
+#endif // SphereObject_h__
